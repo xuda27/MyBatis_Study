9e942aec3e1e151fef8152857064f01771e869cb
xuda <xuda27@163.com>
2016-09-07 13:36:37 +0800

    第一次提交

---
+++ src/io/xuda/beans/Users.java
@@ -0,0 +1,48 @@
+package io.xuda.beans;
+
+import java.util.Date;
+
+public class Users {
+	private int id;
+	private String name;
+	private String password;
+	private String email;
+	private Date birthday;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getBirthday() {
+		return birthday.toLocaleString();
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	@Override
+	public String toString() {
+		return "Users [id=" + id + ", name=" + name + ", password=" + password
+				+ ", email=" + email + ", birthday=" + birthday + "]";
+	}
+	
+	
+}
---
+++ src/io/xuda/test/Test.java
@@ -0,0 +1,48 @@
+package io.xuda.test;
+
+import io.xuda.beans.Users;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class Test {
+
+	public static void main(String[] args) {
+		//mybatis的配置文件
+		String resource = "conf.xml";
+		//使用类加载器加载mybatis的配置文件 (它也加载关联的映射文件)
+//		InputStream is = Test.class.getClassLoader().getResourceAsStream(resource);
+		
+		//构建sqlSession的工厂 
+//		SqlSessionFactory sessionFactory =new SqlSessionFactoryBuilder().build(is);
+		Reader reader=null;
+		try {
+			reader = Resources.getResourceAsReader(resource);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		SqlSessionFactory sessionFactory =new SqlSessionFactoryBuilder().build(reader);
+		
+		SqlSession session = sessionFactory.openSession();
+		
+		String statement = "io.xuda.mapping.userMapper.getUser";
+		
+		
+		Users user =null;
+		try {
+			user = session.selectOne(statement,2);
+		} finally{
+			session.close();
+		}
+		
+		System.out.println(user);
+		
+	}
+
+}
